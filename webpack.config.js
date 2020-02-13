621f7ab673f3ee029efe85d28196357e4870f002
hearsay316 <hearsay316@gmail.com>
2020-02-14 01:30:55 +0800

    项目初始化

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require("path")
+const htmlWebpackPlugin = require("html-webpack-plugin")
+module.exports = {
+    entry:"./src/index.js" , // 以src配置下的目录
+    output:{
+        filename:"bundle.js",
+        path:path.resolve(__dirname,"dist")
+    },
+    devtool:'source-map' ,
+    plugins:[
+        new htmlWebpackPlugin({
+            template:path.resolve(__dirname,"public/index.html")
+        })
+    ]
+}
+console.log(path.resolve(__dirname, "public/index.html"));
