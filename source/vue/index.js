d72bb8298f060f7d335a8913fe54468fc93af23e
hearsay316 <hearsay316@gmail.com>
2020-02-15 11:12:50 +0800

    项目初始化

--- source/vue/index.js
+++ source/vue/index.js
@@ -1,5 +1,13 @@
-export default class Vue {
-    constructor() {
-        console.log(111)
-    }
+
+function Vue(options) {
+    this_init(options)
 }
+Vue.prototype._init = function(options){
+    // Vue初始化  this.$options 表示Vue中的参数
+    let vm = this;
+    vm.$options = options;
+    // Mvvm 原理
+
+}
+
+export default Vue
